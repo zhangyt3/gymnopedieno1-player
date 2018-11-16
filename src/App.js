227dc710b46df26b02ab8089eb5218899250e65d
Kenny Zhang <busstopbench@gmail.com>
2018-11-16 16:25:46 -0600

    Start audio on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import "./App.css";
+import './App.css';
 
-import React, { Component } from "react";
-import Sound from "react-sound";
-import gymnopedieMp3 from "./assets/gymnopedieno1.mp3";
+import React, {Component} from 'react';
+import Sound from 'react-sound';
+import gymnopedieMp3 from './assets/gymnopedieno1.mp3';
 
 class App extends Component {
-  state = { audioPosition: 0 };
+  state = {started: false, audioPosition: 0};
+
+  startAudio = () => {
+    if (!this.state.started) {
+      this.setState({started: true});
+    }
+  };
 
   resetAudio = () => {
-    this.setState({ audioPosition: 0 });
+    this.setState({audioPosition: 0});
   };
 
   render() {
     return (
-      <Sound
-        url={gymnopedieMp3}
-        autoPlay={true}
-        playFromPosition={this.state.audioPosition}
-        playStatus={Sound.status.PLAYING}
-        onFinishedPlaying={this.resetAudio}
-      />
+      <div>
+        <button onClick={this.startAudio}>Start</button>
+        <Sound
+          url={gymnopedieMp3}
+          playFromPosition={this.state.audioPosition}
+          playStatus={
+            this.state.started ? Sound.status.PLAYING : Sound.status.PAUSED
+          }
+          onFinishedPlaying={this.resetAudio}
+        />
+      </div>
     );
   }
 }
